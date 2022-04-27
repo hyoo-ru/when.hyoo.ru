508020b2ec4aaa5250937a9e6182279fc672bc40
nin-jin <nin-jin@users.noreply.github.com>
2022-04-27 07:22:25 +0000

    Deployed using Blazing fast GitHub Pages deploy

--- node.js
+++ node.js
@@ -10098,7 +10098,7 @@ var $;
                 return val;
             return "";
         }
-        hint() {
+        input_hint() {
             return "YYYY-MM-DD hh:mm";
         }
         enabled() {
@@ -10107,7 +10107,7 @@ var $;
         Input() {
             const obj = new this.$.$mol_string();
             obj.value = (val) => this.value(val);
-            obj.hint = () => this.hint();
+            obj.hint = () => this.input_hint();
             obj.enabled = () => this.enabled();
             return obj;
         }
